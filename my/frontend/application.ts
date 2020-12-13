d22f2fd003d881dc070636590a566ac56c51448b
Lunar <lunar@forsaken-borders.net>
2020-12-12 20:48:21 -0600

    Started on 'my' subdomain

---
+++ my/frontend/application.ts
@@ -0,0 +1,36 @@
+class Cookie {
+    public static Get(name: string): string | null {
+        let cookies: string[] = document.cookie.split(';');
+        cookies.forEach(cookie => {
+            let cookieContent = cookie.split('=');
+            if (cookieContent[0] == name) return decodeURIComponent(cookieContent[1]);
+        });
+        return null;
+    }
+
+    public static Set(name: string, content: any, hoursTilExpire: number): void {
+        document.cookie.concat(`${name.trim()}=${encodeURIComponent(content)}; max-age=${hoursTilExpire * 60 * 60};path=/;domain=my.forsaken-borders.net;secure`);
+    }
+
+    public static Update(name: string, content: any, hoursTilExpire: number): void {
+        let cookies: string[] = document.cookie.split(';');
+        for (let i: number = 0; i < cookies.length; i++) {
+            let cookieContent = cookies[i].split('=');
+            if (cookieContent[0].toLowerCase() == name.toLowerCase()) {
+                cookies[i] = `${name.trim()}=${encodeURIComponent(content)}; max-age=${hoursTilExpire * 60 * 60};path=/;domain=my.forsaken-borders.net;secure`;
+                break;
+            }
+        }
+    }
+
+    public static Remove(name: string): void {
+        let cookies: string[] = document.cookie.split(';');
+        for (let i: number = 0; i < cookies.length; i++) {
+            let cookieContent = cookies[i].split('=');
+            if (cookieContent[0].toLowerCase() == name.toLowerCase()) {
+                delete cookies[i];
+                break;
+            }
+        }
+    }
+}
